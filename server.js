ab6e79baecad5dc9b2bb1265d0e538ee93121bb7
AmirrezaNoori <amirrezwanoori@gmail.com>
2026-01-29 15:57:12 +0330

    add controllers and sevices

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const port = 3000;
 
 const contactController = require('./src/controllers/contactController');
 
-// Swagger options
 const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
@@ -16,26 +15,20 @@ const swaggerOptions = {
       description: 'A simple Express API for managing contacts',
     },
   },
-  // مسیرهایی که Swagger باید داکیومنت کنه
   apis: ['./src/controllers/contactController.js'],
 };
 
-// Swagger setup
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
 
-// Middleware برای نمایش Swagger UI در `/api-docs`
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-// Middleware برای خواندن body به فرمت JSON
 app.use(express.json());
 
-// روت‌ها
 app.post('/contacts', contactController.createContact);
 app.get('/contacts', contactController.getContacts);
 app.put('/contacts/:id', contactController.updateContact);
 app.delete('/contacts/:id', contactController.deleteContact);
 
-// شروع سرور
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
   console.log(`Swagger docs available at http://localhost:${port}/api-docs`);
